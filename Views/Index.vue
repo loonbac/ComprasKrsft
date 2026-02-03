ca0c00c17e39c014b8ca43a9fab01d2cc1359b2d
Joshua Rosales <joshuarosalesmoreno@hotmail.com>
2026-02-03 13:49:28 -0500

    fix: move Quick Pay button to Por Pagar section

--- Views/Index.vue
+++ Views/Index.vue
@@ -157,6 +157,16 @@
 
         <!-- TAB: Por Pagar - Listas por aprobación -->
         <template v-if="activeTab === 'to_pay'">
+          <!-- Pago Rápido Button -->
+          <div class="quick-pay-section">
+            <button @click="openQuickPayModal" class="btn-quick-pay">
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
+              </svg>
+              Pago Rápido
+            </button>
+          </div>
+
           <!-- Loading -->
           <div v-if="loading" class="loading-container">
             <div class="loading-spinner"></div>
@@ -220,16 +230,6 @@
 
         <!-- TAB: Pagadas -->
         <template v-if="activeTab === 'paid'">
-          <!-- Pago Rápido Button -->
-          <div class="quick-pay-section">
-            <button @click="openQuickPayModal" class="btn-quick-pay">
-              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
-              </svg>
-              Pago Rápido
-            </button>
-          </div>
-
           <!-- Filter and Export Bar -->
           <div v-if="paidBatches.length > 0" class="export-bar">
             <!-- Date Filter -->
